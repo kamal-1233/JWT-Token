a5b34a93aa0e40c80fd102e7a2b15d982f007c49
kamal-1233 <emosharma754@gmail.com>
2023-07-07 15:58:34 +0530

    last changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,6 +23,18 @@ const registerUser = asyncHandler (async (req,res) => {
 
 const hashedPassword = await bcrypt.hash(password, 10);
 console.log("Hashed password:", hashedPassword);
+const user = await User.create({
+   username,
+   email,
+   password: hashedPassword, 
+});
+console.log(`User created ${user}`);
+if (user) {
+    res.status(201).json ({_id: user.id, email: user.email});
+} else{
+    res.status(400);
+    throw new Error ("User data us not valid");
+}
     res.json({message: "Register the user"});
  });
 
