7fd062e7bc9da29cd8de8ea991b13a979b8ca668
kamal-1233 <emosharma754@gmail.com>
2023-07-10 12:58:35 +0530

    new edit

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,50 +3,51 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
-// GET Register a user
-// route POST /api/users/register
-// access public
-
+//@desc Register a user
+//@route POST /api/users/register
+//@access public
 const registerUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
   if (!username || !email || !password) {
     res.status(400);
-    throw new Error("All fields are mandatory");
+    throw new Error("All fields are mandatory!");
   }
   const userAvailable = await User.findOne({ email });
   if (userAvailable) {
     res.status(400);
-    throw new Error("User already registered");
+    throw new Error("User already registered!");
   }
 
-  // Hash password
+  //Hash password
   const hashedPassword = await bcrypt.hash(password, 10);
-  console.log("Hashed password:", hashedPassword);
+  console.log("Hashed Password: ", hashedPassword);
   const user = await User.create({
     username,
     email,
     password: hashedPassword,
   });
+
   console.log(`User created ${user}`);
   if (user) {
     res.status(201).json({ _id: user.id, email: user.email });
   } else {
     res.status(400);
-    throw new Error("User data is not valid");
+    throw new Error("User data us not valid");
   }
+  res.json({ message: "Register the user" });
 });
 
-// GET login a user
-// route POST /api/users/login
-// access public
-
+//@desc Login user
+//@route POST /api/users/login
+//@access public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
     res.status(400);
-    throw new Error("All fields are mandatory");
+    throw new Error("All fields are mandatory!");
   }
   const user = await User.findOne({ email });
+  //compare password with hashedpassword
   if (user && (await bcrypt.compare(password, user.password))) {
     const accessToken = jwt.sign(
       {
@@ -56,22 +57,21 @@ const loginUser = asyncHandler(async (req, res) => {
           id: user.id,
         },
       },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "1m" }
+      process.env.ACCESS_TOKEN_SECERT,
+      { expiresIn: "15m" }
     );
     res.status(200).json({ accessToken });
   } else {
     res.status(401);
-    throw new Error("Email or password is not valid");
+    throw new Error("email or password is not valid");
   }
 });
 
-// GET Current a user
-// route POST /api/users/current
-// access private
-
+//@desc Current user info
+//@route POST /api/users/current
+//@access private
 const currentUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Current user information" });
+  res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, currentUser };
